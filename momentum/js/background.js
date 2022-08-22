75f6e17420a371f63913d1a31dbe89abda3b47b5
Seounghyun Na <80561363+NaSeonghyen@users.noreply.github.com>
2022-08-23 08:48:14 +0900

    JS Image -> body background-image

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -2,10 +2,15 @@ const images = ["0.jpg", "1.jpg", "2.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
+const BACKGROUNDIMAGE = `img/${chosenImage}`;
 
-bgImage.src = `img/${chosenImage}`;
+document.body.style.backgroundImage = `url(${BACKGROUNDIMAGE})`;
 
-document.body.appendChild(bgImage);
-console.log(bgImage);
+// 2022.08.23 NSH 이미지 배경처리로 인한 주석처리
+
+// const bgImage = document.createElement("img");
+// bgImage.src = BACKGROUNDIMAGE;
+//document.body.appendChild(bgImage);
+
+
 
