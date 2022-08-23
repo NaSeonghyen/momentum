70ee5a7fb02f8ab035e1164024309312d68e1d00
Seounghyun Na <80561363+NaSeonghyen@users.noreply.github.com>
2022-08-23 15:16:57 +0900

    Weather API added

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,17 @@
+const API_KEY = "8451d290dfcbd2e1f0d9f8377c651fc4";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("You live in", lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+            city.innerText = name
+    });
 }
 function onGeoError() {
     alert("Can't find you. No weather for you.");
