dd768db7c38e2455a7e35f50dcaa44fd1c04cc1a
Seounghyun Na <80561363+NaSeonghyen@users.noreply.github.com>
2022-08-24 11:01:13 +0900

    배경 이미지 추가 및 배경 설정
    1) 배경이미지 1개 변경 및 2개 추가
    2) 배경 설정
    - 브라우저 크기에 맞게 변경
    - 배경이미지 반복 못하게 변경
    3) 날씨 현재 위치 정보 오류 수정

--- js/weather.js
+++ js/weather.js
@@ -10,7 +10,7 @@ function onGeoOk(position) {
             const weather = document.querySelector("#weather span:first-child");
             const city = document.querySelector("#weather span:last-child");
             weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-            city.innerText = name
+            city.innerText = data.name;
     });
 }
 function onGeoError() {
