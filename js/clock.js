23f461709f25f142aeaaa76cf93bad603edac9a2
Seounghyun Na <80561363+NaSeonghyen@users.noreply.github.com>
2022-08-25 16:49:21 +0900

    명언 변경, 이전 기능 추가

--- js/clock.js
+++ js/clock.js
@@ -4,8 +4,8 @@ const dateYMD = document.querySelector("#date");
 function getClock() {
     const date = new Date();
     const year = date.getFullYear();
-    const month = date.getMonth();
-    const day = date.getDay();
+    const month = date.getMonth()+1;
+    const day = date.getDate();
     const hours = String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
--- js/quotes.js
+++ js/quotes.js
@@ -1,43 +1,43 @@
 const quotes = [
     {
-        quote: "When you feel like giving up, you are close to success.",
-        author: "Bob Parsons",
+        quote: "강한자가 살아남는 것이 아니라 살아남는 자가 강한 것이다.",
+        author: "김유신",
     },
     {
-        quote: "Recording dreams has never been the goal. My goal is to make my dream come true.",
-        author: "Manlei",
+        quote: "세월을 헛되이 보내지 마라, 청춘은 다시 돌아오지 않는다.",
+        author: "안중근",
     },
     {
-        quote: "The greatest competitiveness is passion.",
-        author: "Jack Welch",
+        quote: "진실은 반드시 따르는 자가 있고 정의는 반드시 이루는 날이 있다.",
+        author: "안창호",
     },
     {
-        quote: "I have to go beyond my life like myself. That's how I live my life as master.",
-        author: "Senda Takuya",
+        quote: "삶이 있는 한 희망은 있다",
+        author: "키케로",
     },
     {
-        quote: "I failed 9 times out of 10 attempts. So I tried 10 times.",
-        author: "George Bernard Shaw",
+        quote: "산다는것 그것은 치열한 전투이다.",
+        author: "로망로랑",
     },
     {
-        quote: "The only thing we really own is time. Even those who have nothing else have time.",
-        author: "Balthasar Gracian",
+        quote: "하루에 3시간을 걸으면 7년 후에 지구를 한바퀴 돌 수 있다.",
+        author: "사무엘존슨",
     },
     {
-        quote: "Enjoy your own life. Don't compare your life to others.",
-        author: "Con d'Orsay",
+        quote: "진정으로 웃으려면 고통을 참아야하며 , 나아가 고통을 즐길 줄 알아야 해",
+        author: "찰리 채플린",
     },
     {
-        quote: "Tomorrow we hope we have learned something from yesterday.",
-        author: "John Wayne",
+        quote: "직업에서 행복을 찾아라. 아니면 행복이 무엇인지 절대 모를 것이다.",
+        author: "엘버트 허버드",
     },
     {
-        quote: "The secret to getting ahead is to start.",
-        author: "Mark Twain",
+        quote: "신은 용기있는자를 결코 버리지 않는다",
+        author: "켄러",
     },
     {
-        quote: "Luck is what remains after 100% effort.",
-        author: "Langster Coleman",
+        quote: "피할수 없으면 즐겨라",
+        author: "로버트 엘리엇",
     },
 ];
 
