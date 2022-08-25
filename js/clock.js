5aaf4636dc085b0948b644f5ad4a7482f09fa2dd
Seounghyun Na <80561363+NaSeonghyen@users.noreply.github.com>
2022-08-25 12:33:55 +0900

    CSS 작업II
    1) 날짜 추가
    2) 글자크기 수정
    3) 명언, TODOlist 레이아웃 변경

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,15 @@
 const clock = document.querySelector("#clock");
+const dateYMD = document.querySelector("#date");
 
 function getClock() {
     const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDay();
     const hours = String(date.getHours()).padStart(2,"0");
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
+    dateYMD.innerText = `${year}년 ${month}월 ${day}일`;
     clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
